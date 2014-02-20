d0059ae886b967693b8dcc086279943d2308b1e1
mvaivre <m.vaivre@gmail.com>
2014-02-20 18:51:59 +0100

    On arrete Mapbox

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 angular.module('VeLauz', ['ionic'])
 
 .controller('VelauzControl', function($scope) {
-  $scope.tasks = [
-    { title: 'Collect coins' },
-    { title: 'Eat mushrooms' },
-    { title: 'Get high enough to grab the flag' },
-    { title: 'Find the Princess' }
-  ];
+	$scope.tasks = [
+		{ title: 'Collect coins' },
+		{ title: 'Eat mushrooms' },
+		{ title: 'Get high enough to grab the flag' },
+		{ title: 'Find the Princess' }
+	];
+  
+	$scope.toggleMenu = function() {
+		$scope.sideMenuController.toggleLeft();
+	};
+
 });
--- js/map.js
+++ js/map.js
@@ -1,2 +1,15 @@
-var map = L.mapbox.map('map', 'examples.map-9ijuk24y')
-    .setView([40, -74.50], 9);
+var view = new ol.View2D({
+  center: [0, 0],
+  zoom: 2
+});
+
+var map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  tileOptions: {crossOriginKeyword: null},
+  target: 'map',
+  view: view
+});

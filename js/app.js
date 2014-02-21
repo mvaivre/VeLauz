ff5628ce65f147c82e7ec72ec1e98e6fb0a626b5
mvaivre <m.vaivre@gmail.com>
2014-02-21 15:51:40 +0100

    OL3 nouveau test
    Essai avec low-level API ionic

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 angular.module('VeLauz', ['ionic'])
 
 .controller('VelauzControl', function($scope) {
-	$scope.tasks = [
-		{ title: 'Collect coins' },
-		{ title: 'Eat mushrooms' },
-		{ title: 'Get high enough to grab the flag' },
-		{ title: 'Find the Princess' }
-	];
+
   
 	$scope.toggleMenu = function() {
 		$scope.sideMenuController.toggleLeft();
 	};
 
+});
+
+var contentEl = document.getElementById('content');
+var content = new ionic.views.SideMenuContent({
+  el: contentEl
+});
+
+var leftMenuEl = document.getElementById('menu-left');
+var leftMenu = new ionic.views.SideMenu({
+  el: leftMenuEl,
+  width: 270
+});
+
+
+var sm = new ionic.controllers.SideMenuController({
+  content: content,
+  left: leftMenu
 });
--- js/map.js
+++ js/map.js
@@ -1,15 +1,17 @@
-var view = new ol.View2D({
-  center: [0, 0],
-  zoom: 2
-});
+var mapInit = function () {
 
-var map = new ol.Map({
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM()
-    })
-  ],
-  tileOptions: {crossOriginKeyword: null},
-  target: 'map',
-  view: view
-});
+ var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View2D({
+          center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+          zoom: 4
+        })
+      });
+}
+
+mapInit();

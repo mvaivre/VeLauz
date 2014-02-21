4039a87908309ee0354a8a0da9b1561d1c23e93f
mvaivre <m.vaivre@gmail.com>
2014-02-21 16:24:01 +0100

    OL3 sidewindow ne bouge pas..

--- js/app.js
+++ js/app.js
@@ -7,21 +7,23 @@ angular.module('VeLauz', ['ionic'])
 		$scope.sideMenuController.toggleLeft();
 	};
 
+	var contentEl = document.getElementById('content');
+	var content = new ionic.views.SideMenuContent({
+	  el: contentEl,
+	  drag-content: "false"
+	});
+
+	var leftMenuEl = document.getElementById('menu-left');
+	var leftMenu = new ionic.views.SideMenu({
+	  el: leftMenuEl,
+	  width: 270
+	});
+
+
+	var sm = new ionic.controllers.SideMenuController({
+	  content: content,
+	  left: leftMenu
+	});
+
 });
 
-var contentEl = document.getElementById('content');
-var content = new ionic.views.SideMenuContent({
-  el: contentEl
-});
-
-var leftMenuEl = document.getElementById('menu-left');
-var leftMenu = new ionic.views.SideMenu({
-  el: leftMenuEl,
-  width: 270
-});
-
-
-var sm = new ionic.controllers.SideMenuController({
-  content: content,
-  left: leftMenu
-});

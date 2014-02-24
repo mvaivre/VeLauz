596cb4feeea42b4bd9a7d021c5978d5f75eea733
mvaivre <m.vaivre@gmail.com>
2014-02-24 14:38:44 +0100

    La carte apparait enfin!
    Je n'avais pas renseigné la directive dans le module principal!
    PROBLEME: les contrôles de zoom disparaissent lorsque la fenêtre bouge.

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,29 +1,31 @@
-//This controller defines the animations provided by the Ionic framework
-app.controller('ionicController', function($scope) {
-  
-	$scope.toggleMenu = function() {
-		$scope.sideMenuController.toggleLeft();
-	};
+var module = angular.module('map_main_controller',[]);
 
-});
+function createMap() {
+	var map = new ol.Map({
+	    layers: [
+	      new ol.layer.Tile({
+	        source: new ol.source.OSM()
+	      })
+	    ],
+	    renderer: 'canvas',
+	    view: new ol.View2D({
+	      center: [0, 0],
+	      zoom: 2
+	    }),
+	    ol3Logo: false
+	});
+
+	return map;
+};
+
+//The application main controller
+module.controller('mapMainController', ['$scope', function($scope) {
+
+// The main controller creates the OpenLayers map object. The map object
+// is central, as most directives/components need a reference to it.
+  $scope.map = createMap();
+
+}]);
 
 
-//This controller 
-app.controller('mapMainController', function($scope) {
-
-  var map = new ol.Map({
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    renderer: 'canvas',
-    view: new ol.View2D({
-      center: [0, 0],
-      zoom: 2
-    })
-  });
-  $scope.map = map;
-
-});
 
--- app/directives/customMap.js
+++ app/directives/customMap.js
@@ -1,14 +1,19 @@
- 
-app.directive('customMap', function() {
+var module = angular.module('custom_map_directive', []); 
+
+
+module.directive('customMap', function() {
   return {
-          restrict: 'A',   
-          scope: {
-            map: '=map' 
-          },
-          link: function(scope, element, attrs) { //(element c'est l'élement sur lequel ta directive se trouve)
-            //(ici tu plantes tous ce dont tu as besoin...)
+    restrict: 'A',   
+    scope: {
+      map: '=customMapMap' 
+    },
+    link: function(scope, element, attrs) { //(element c'est l'élement sur lequel ta directive se trouve)
+      //(ici tu plantes tous ce dont tu as besoin...)
+      var map = scope.map;
 
+      var view = map.getView();
 
-            map.setTarget(element[0]); //donnes un target à ta map
-          };
-        });      
+      map.setTarget(element[0]); //donnes un target à ta map
+    }
+  };    
+});

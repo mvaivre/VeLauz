197486b1919539e63ecba0f0652443a51ecb75bf
mvaivre <m.vaivre@gmail.com>
2014-02-25 11:09:08 +0100

    7eme...

--- app/app.js
+++ app/app.js
@@ -1,22 +1,6 @@
 /*Here are defined the MAIN module, routes and controller driving the app*/
 /*The other controllers/directives/factories can be found in the app folder*/
 
-//We're manually bootstrapping Angular when PhoneGap fires the 'ondeviceready' event.
-var velauz = {
-    initialize: function() {
-        this.bindEvents();
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, true);
-    },
-
-    onDeviceReady: function() {
-        angular.element(document).ready(function() {
-            angular.bootstrap(document);
-        });
-    },
-};
-
 //Global module, containing the other modules
 var module= angular.module('VeLauz', [
 	'ionic',
--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,32 +1,33 @@
-var module = angular.module('map_main_controller',['ionic']);
+var module = angular.module('map_main_controller',[]);
 
 
 //document.addEventListener("deviceready", createMap, false); //to work with PhoneGap (hopefully)
-	function createMap() {
-		
-		var map = new ol.Map({
-			layers: [
-			new ol.layer.Tile({
-				source: new ol.source.OSM()
-			})
-			],
-			renderer: 'canvas',
-			view: new ol.View2D({
-				center: ol.proj.transform([6.63, 56.52], 'EPSG:4326', 'EPSG:3857'),
-				zoom: 7
-			}),
-			ol3Logo: false
-		});
+function createMap() {
 
-		return map;
-	};
+	
+
+	return map;
+};
 
 //The application main controller
 module.controller('mapMainController', ['$scope', function($scope, Plateform) {
 // The main controller creates the OpenLayers map object. The map object
 // is central, as most directives/components need a reference to it.
+	var map = new ol.Map({
+		layers: [
+		new ol.layer.Tile({
+			source: new ol.source.OSM()
+		})
+		],
+		renderer: 'canvas',
+		view: new ol.View2D({
+			center: ol.proj.transform([6.63, 56.52], 'EPSG:4326', 'EPSG:3857'),
+			zoom: 7
+		}),
+		ol3Logo: false
+	});
 
-		$scope.map = createMap();
+	$scope.map = map;
 }]);
 
 

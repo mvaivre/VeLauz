ed8ae62441006076504e191f88719f2ba63277bf
mvaivre <m.vaivre@gmail.com>
2014-02-25 11:24:16 +0100

    8eme

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -2,32 +2,31 @@ var module = angular.module('map_main_controller',[]);
 
 
 //document.addEventListener("deviceready", createMap, false); //to work with PhoneGap (hopefully)
-function createMap() {
+	function createMap() {
+		
+		var map = new ol.Map({
+			layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()
+			})
+			],
+			renderer: 'canvas',
+			view: new ol.View2D({
+				center: ol.proj.transform([6.63, 56.52], 'EPSG:4326', 'EPSG:3857'),
+				zoom: 7
+			}),
+			ol3Logo: false
+		});
 
-	
-
-	return map;
-};
+		return map;
+	};
 
 //The application main controller
 module.controller('mapMainController', ['$scope', function($scope, Plateform) {
 // The main controller creates the OpenLayers map object. The map object
 // is central, as most directives/components need a reference to it.
-	var map = new ol.Map({
-		layers: [
-		new ol.layer.Tile({
-			source: new ol.source.OSM()
-		})
-		],
-		renderer: 'canvas',
-		view: new ol.View2D({
-			center: ol.proj.transform([6.63, 56.52], 'EPSG:4326', 'EPSG:3857'),
-			zoom: 7
-		}),
-		ol3Logo: false
-	});
-
-	$scope.map = map;
+	
+		$scope.map = document.addEventListener('deviceready', createMap, false);
 }]);
 
 

02a740d5a136732dbd46c3c737d1c85012d71103
mvaivre <m.vaivre@gmail.com>
2014-02-25 19:07:13 +0100

    Geolocalisation, 1er test

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -4,7 +4,7 @@ var VeLauz = angular.module('map_main_controller',[]);
 
 
 //The application main controller
-VeLauz.controller('mapMainController', ['$scope', function($scope) {
+VeLauz.controller('mapMainController', function ($scope, geolocation) {
 // The main controller creates the OpenLayers map object. The map object
 // is central, as most directives/components need a reference to it.
 var map = new ol.Map({
@@ -15,14 +15,29 @@ var map = new ol.Map({
 	],
 	renderer: 'canvas',
 	view: new ol.View2D({
-		center: ol.proj.transform([6.65, 46.50], 'EPSG:4326', 'EPSG:3857'),
+		center: ol.proj.transform([6.634, 46.519], 'EPSG:4326', 'EPSG:3857'),
 		zoom: 12
 	}),
-	ol3Logo: false
+	ol3Logo: false,
+	controls: []
 });
 
 $scope.map = map;
-}]);
+
+//Geolocalisation
+ geolocation.getCurrentPosition(function (position) {
+    alert('Latitude: '              + position.coords.latitude          + '\n' +
+          'Longitude: '             + position.coords.longitude         + '\n' +
+          'Altitude: '              + position.coords.altitude          + '\n' +
+          'Accuracy: '              + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: '     + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '               + position.coords.heading           + '\n' +
+          'Speed: '                 + position.coords.speed             + '\n' +
+          'Timestamp: '             + position.timestamp                + '\n');
+  });
+
+});
+
 
 
 

58f06d50198233a04846f39d696e7883df97f409
mvaivre <m.vaivre@gmail.com>
2014-02-25 09:46:31 +0100

    5eme

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -9,27 +9,26 @@ module.controller('mapMainController', ['$scope', function($scope, Plateform) {
 // The main controller creates the OpenLayers map object. The map object
 // is central, as most directives/components need a reference to it.
 
-	ionic.Platform.ready(function createMap() { //to work with PhoneGap (hopefully)
+	function createMap() {
 		
 		var map = new ol.Map({
-		    layers: [
-		      new ol.layer.Tile({
-		        source: new ol.source.OSM()
-		      })
-		    ],
-		    renderer: 'canvas',
-		    view: new ol.View2D({
-		      center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
-		      zoom: 7
-		    }),
-		    ol3Logo: false
+			layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()
+			})
+			],
+			renderer: 'canvas',
+			view: new ol.View2D({
+				center: ol.proj.transform([6.63, 56.52], 'EPSG:4326', 'EPSG:3857'),
+				zoom: 7
+			}),
+			ol3Logo: false
 		});
 
 		return map;
-	});
+	};
 
-  $scope.map = createMap();
-
+		$scope.map = createMap();
 }]);
 
 
